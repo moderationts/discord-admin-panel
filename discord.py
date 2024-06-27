ce529d6cd8f446ad262dd5f75cb442bb53c23035
James <144442238+moderationts@users.noreply.github.com>
2024-06-27 08:04:32 +0200

    Update discord.py

--- discord.py
+++ discord.py
@@ -3,7 +3,7 @@ import requests
 class Discord:
     def __init__(self):
         self.base_url = "https://discord.com/api/v9/"
-        self.alt_token = "
+        self.alt_token = "insert discord user token here"
     def get(self,url,headers={}):
         headers['Authorization'] = self.alt_token
         print(headers)
